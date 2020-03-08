e51fd519cb9c8504e335292d9f2482de0927d45c
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-03-08 19:37:51 +0100

    Update function.py

--- lib/function.py
+++ lib/function.py
@@ -1,9 +1,86 @@
-#coding:utf-8
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+from urllib.request import *
+import urllib.error
+from urllib.parse import urlparse, parse_qs
 
-# Fonction qui permet de injecter un payload dans chaque paramètres de l'url tout en conservant ces valeurs d'origine
-# Retourne une liste
+# Retourne le code http reponse d'un url
+def get_code(url):
+
+	reponse_code = ''
+	try:
+		r = urlopen(url)
+	except urllib.error.HTTPError as e:
+		reponse_code = e.code
+	else:
+		reponse_code = r.getcode()
+
+	return reponse_code
+
+# Vérifie une url avec des paramètres et des valeurs déclarer
+# Exemple : http://example.com/?id=1&page=forum => Valid
+# 		  :	http://example.com/?id=&page=forum 	=> Invalid
+# Retourn true si l'url est valid
+def verify_url_with_query_valid(url):
+
+	valid_url = False
+
+	urlparsed = urlparse(url)# On parse l'url pour récupérer ses composants
+
+	protocol = urlparsed.scheme # Récupération du protocol de l'url
+	domain = urlparsed.netloc # Récupartion de l'hôte de l'url
+	query = urlparsed.query # Récupération du query de l'url
+	para_and_value = parse_qs(urlparsed.query) # Récupération des paramètres et des valeurs de l'url
+
+	if protocol == 'http' or protocol == 'https': # On vérifie sur le protocole est bien http ou https
+		if domain is not None:# Que l'hôte est présent
+			if query is not None:# Que les querys sont présent
+				if len(para_and_value) > 0:# Que les paramètres et les valeurs sont présent
+					valid_url = True
+					return valid_url
+				else:
+					print('[!] No query found into the URL')
+					valid_url = False
+					return valid_url
+			else:
+				print('[!] No query found into the URL')
+				valid_url = False
+				return valid_url
+		else:
+			print('[!] Invalid URL')
+			valid_url = False
+			return valid_url
+	else:
+		print('[!] You must specify http or https into the URL')
+		valid_url = False
+		return valid_url
+
+# Vérifie une url avec un mot-clées, ici c'est <xss>
+# Retourne true si l'url est valide
+def verify_url_with_a_keyword(url):
+
+	valid_url = False
+
+	urlparsed = urlparse(url)# On parse l'url pour récupérer ses composants
+	protocol = urlparsed.scheme # Récupération du protocol de l'url
+	domain = urlparsed.netloc # Récupartion de l'hôte de l'url
+
+	if protocol == 'http' or protocol == 'https':
+		if domain is not None:
+			if '<xss>' in url:
+				valid_url = True
+				return valid_url
+			else:
+				print('[!] You must specify the keyword <xss> into the URL')
+				valid_url = False
+				return valid_url
+		else:
+			print('[!] Invalid URL')
+			valid_url = False
+			return valid_url
+	else:
+		print('[!] You must specify http or https into the URL')
+		valid_url = False
+		return valid_url
+
 def inject(URL, PAYLOAD):
 
 	urlparsed = urlparse(URL);# Création d'un object urlparse
